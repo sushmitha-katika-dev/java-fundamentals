87c4cfd905075df3bc6496013f54e231a5fd2444
SushmithaKatika <katikasushmitha7228@gmail.com>
2026-02-25 19:54:24 +0530

    initial commit

---
+++ src/com/java/fundamentals/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.java.fundamentals;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Hi This is my first java code");
+    }
+}
