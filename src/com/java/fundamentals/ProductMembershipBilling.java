4b4bd98ddc21464b8e0d90dff21724e2c69140ac
SushmithaKatika <katikasushmitha7228@gmail.com>
2026-03-08 10:05:28 +0530

    product membership billing

--- src/com/java/fundamentals/ProductMembershipBilling.java
+++ src/com/java/fundamentals/ProductMembershipBilling.java
@@ -11,54 +11,86 @@ public class ProductMembershipBilling {
         String email = scanner.next();
         System.out.println("Enter phone number");
         float phoneNumber = scanner.nextFloat();
-        System.out.println("Enter Membership");
+        System.out.println("Enter Membership(S/G/D)");
         char membership = scanner.next().charAt(0);
         System.out.println("Enter Age");
         byte age = scanner.nextByte();
-        System.out.println("------*** Customer Details ***-------");
-        System.out.println("Name : "+name);
-        System.out.println("Email : "+email);
-        System.out.println("Phone Nummber : "+phoneNumber);
-        System.out.println("Membership : "+membership);
-        System.out.println("Age : "+age);
-        System.out.println("---*** Product Membership Billing System ***---");
+
         int produtPrice = 10000;
         byte cgst = 5;
         byte sgst = 5;
+        double shipping = 0;
+        System.out.println("Enter quantity");
+        byte quantity = scanner.nextByte();
+        
         double discountPercentage = 0;
         if(age > 50){
             if (membership == 'G' || membership == 'g') {
                 discountPercentage = 50;
+                shipping = 20;
             }
             if(membership == 'S' || membership == 's'){
                 discountPercentage = 40;
+                shipping = 50;
             }
             if (membership == 'D' || membership == 'd'){
                 discountPercentage = 60;
+                shipping = 0;
             }
         }else {
             discountPercentage = 10;
+            shipping = 100;
         }
 
-        System.out.println("Product Price : "+produtPrice);
-        System.out.println("Discount % : "+discountPercentage+"%");
-
+        //discount calculatio
         double discountPrice = (discountPercentage*produtPrice)/100;
-        System.out.println("Discount Price : "+discountPrice);
-
         double discountedPrice = produtPrice - discountPrice;
+        
+        //Tax calculation
+        double cgstAmount = (discountedPrice * cgst) /100;
+        double sgstAmount = (discountedPrice * sgst)/100;
+        
+        double finalPricePerProduct = discountedPrice + cgstAmount + sgstAmount;
+
+        // Final bill
+        double finalBill = (finalPricePerProduct * quantity) + shipping;
+
+        double totalSavings = discountPrice * quantity;
+
+        System.out.println("\n========= BILL SUMMARY =========");
+
+        System.out.println("\n========= CUSTOMER DETAILS =========");
+        System.out.println("Name : "+name);
+        System.out.println("Email : "+email);
+        System.out.println("Phone Number : "+phoneNumber);
+        System.out.println("Membership : "+membership);
+        System.out.println("Age : "+age);
+        System.out.println("-------------------------------");
+
+        System.out.println("===== PRODUCT MEMBERSHIP BILLING SYSTEM =====");
+
+        System.out.println("Product Price : "+produtPrice);
+        System.out.println("Quantity : " + quantity);
+
+        System.out.println("Discount % : "+discountPercentage+"%");
+        System.out.println("Discount Price : "+discountPrice);
         System.out.println("Discounted Price : "+discountedPrice);
 
-        double cgstAmount = (discountedPrice * cgst) /100;
+
         System.out.println("CGST 5% : "+ cgstAmount);
-
-        double sgstAmount = (discountedPrice * sgst)/100;
         System.out.println("SGST 5% : "+ sgstAmount);
+        System.out.println("Shipping Charges : " + shipping);
 
-        double finalPrice = discountedPrice + cgstAmount + sgstAmount;
-        System.out.println("Final Bill : "+finalPrice);
+        System.out.println("-------------------------------");
 
-        System.out.println("------ ******** ------");
+        System.out.println("Final Price Per Product : " + finalPricePerProduct);
+        System.out.println("Final Bill Amount : " + finalBill);
+
+        System.out.println("Total Savings : ₹" + totalSavings);
+
+        System.out.println("================================");
+
+        scanner.close();
 
     }
 }
